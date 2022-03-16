1bfaa666756adac736f4705df95c8fea0ab36780
Sahar Zigdon <saharzigdon@Sahars-MBP.lan>
2022-03-16 14:54:12 +0200

    responsive design

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,4 +1,6 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faX } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import HeroVideo from "../assets/videos/video.mp4"
@@ -6,6 +8,7 @@ import jsonData from "../data/data.json"
 
 export function AppHeader() {
   const [navbar, setNavbar] = useState(false)
+  const [hamburgerNav, setHamburgerNav] = useState(false)
 
   const changeBackground = () => {
     if (window.scrollY >= 50) {
@@ -15,14 +18,30 @@ export function AppHeader() {
     }
   }
 
+  const toggleHamburger = () => {
+    setHamburgerNav(!hamburgerNav)
+  }
+
   window.addEventListener("scroll", changeBackground)
 
   return (
     <header className="main-header">
-      <video autoPlay loop muted>
-        <source src={HeroVideo} type="video/mp4"/>
+      <FontAwesomeIcon
+        onClick={toggleHamburger}
+        className="hamburger"
+        color="white"
+        icon={faBars}
+      />
+      <video autoPlay loop>
+        <source src={HeroVideo} type="video/mp4" />
       </video>
-      <nav className={navbar ? "active-navbar" : ""}>
+      <nav className={navbar ? "active-navbar" : "" || hamburgerNav ? "active-hamburger" : ""}>
+        <FontAwesomeIcon
+          onClick={toggleHamburger}
+          className="close-icon"
+          color="white"
+          icon={faX}
+        />
         <NavLink to="/">ראשי</NavLink>
         <NavLink to="/about">אודותינו</NavLink>
         <NavLink to="/contact">צור קשר</NavLink>
@@ -32,11 +51,11 @@ export function AppHeader() {
         <h2>{jsonData.hero.title1}</h2>
         <h1>{jsonData.hero.title2}</h1>
         <div className="line"></div>
-        <p>
-        {jsonData.hero.peregraph}
-        </p>
+        <p>{jsonData.hero.peregraph}</p>
         <button>ספרו לי עוד</button>
       </section>
     </header>
   )
 }
+
+//
